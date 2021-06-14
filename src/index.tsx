828d66de7b6d105cfe147eae08802db820b30d99
rickhuang <rick.liyue.huang@gmail.com>
2021-06-14 19:40:39 +1000

    feat: import dev-tool to realize backend

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,14 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { loadDevTools } from "jira-dev-tool";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+loadDevTools(() => {
+  return ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
