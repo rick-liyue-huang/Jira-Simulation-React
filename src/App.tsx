a9b7e5090905d5236b9303eb545a62a11ccf0822
rickhuang <rick.liyue.huang@gmail.com>
2021-06-14 19:33:42 +1000

    feat: using middlewares in json-server to realize backend

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { ProjectList } from "./screens/projectList";
+import { LoginScreen } from "./screens/login";
 
 function App() {
   return (
     <div className="App">
-      <ProjectList />
+      {/*<ProjectList />*/}
+      <LoginScreen />
     </div>
   );
 }
---
+++ src/screens/login/index.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { FormEvent } from "react";
+
+const apiURL = process.env.REACT_APP_API_URL;
+
+export const LoginScreen = () => {
+  const handleLogin = (param: { username: string; password: string }) => {
+    fetch(`${apiURL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(param),
+    }).then(async (res) => {
+      if (res.ok) {
+      }
+    });
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    const username = (evt.currentTarget.elements[0] as HTMLInputElement).value;
+    const password = (evt.currentTarget.elements[1] as HTMLInputElement).value;
+    handleLogin({ username, password });
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">用户名</label>
+        <input type="text" id={"username"} />
+      </div>
+      <div>
+        <label htmlFor="password">密码</label>
+        <input type="password" id={"password"} />
+      </div>
+      <button type={"submit"}>提交</button>
+    </form>
+  );
+};
