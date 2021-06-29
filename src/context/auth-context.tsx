edf5480064b25dbda058cc86e473a5fa9bcd630b
rickhuang <rick.liyue.huang@gmail.com>
2021-06-29 12:12:53 +1000

    feat: add http and useHttp to deal with token

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,11 +1,26 @@
 import React, { ReactNode, useState } from "react";
 import * as auth from "auth-providers";
 import { User } from "../screens/projectList/search-panel";
+import { http } from "../util/http";
+import { useMount } from "../util";
 
 interface AuthForm {
   username: string;
   password: string;
 }
+
+// 在登录状态刷新后仍然登录，
+// 这是一个异步请求需要时间，因此这个页面会从未登录变为登录
+const bootstrapUser = async () => {
+  let user = null;
+  const token = auth.getToken();
+  if (token) {
+    // 这里使用http 是为了自己指定token
+    const data = await http("me", { token });
+    user = data.user;
+  }
+  return user;
+};
 
 const AuthContext = React.createContext<
   | {
@@ -26,6 +41,11 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     auth.login(form).then((user) => setUser(user));
   const register = (form: AuthForm) => auth.register(form).then(setUser);
   const logout = () => auth.logout().then(() => setUser(null));
+
+  //页面加载的时候，调用这个方法，保证处于登录状态，如果有token的话
+  useMount(() => {
+    bootstrapUser().then(setUser);
+  });
 
   return (
     <AuthContext.Provider
--- src/screens/projectList/index.tsx
+++ src/screens/projectList/index.tsx
@@ -3,6 +3,7 @@ import { List } from "./list";
 import { useEffect, useState } from "react";
 import * as qs from "qs";
 import { useDebounce, cleanObject, useMount } from "../../util";
+import { useHttp } from "../../util/http";
 
 const apiURL = process.env.REACT_APP_API_URL;
 
@@ -15,22 +16,29 @@ export const ProjectListScreen = () => {
   const [list, setList] = useState([]);
   const debouncedParam = useDebounce(param, 2000);
 
+  // 使用 自定义 http hook, 这样可以将token传入到登录后的项目展示列表中
+  const client = useHttp();
+
   useEffect(() => {
-    fetch(
+    client("projects", { data: cleanObject(debouncedParam) }).then(setList);
+
+    /*fetch(
       `${apiURL}/projects?${qs.stringify(cleanObject(debouncedParam))}`
     ).then(async (response) => {
       if (response.ok) {
         setList(await response.json());
       }
-    });
+    });*/
   }, [debouncedParam]);
 
   useMount(() => {
-    fetch(`${apiURL}/users`).then(async (response) => {
+    client("users").then(setUsers);
+
+    /*fetch(`${apiURL}/users`).then(async (response) => {
       if (response.ok) {
         setUsers(await response.json());
       }
-    });
+    });*/
   });
 
   return (
