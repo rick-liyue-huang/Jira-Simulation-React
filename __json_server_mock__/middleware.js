23827b6cc021d40ed358b69b1355e5f2923e3e35
rickhuang <rick.liyue.huang@gmail.com>
2021-06-29 10:16:11 +1000

    feat: apply middlewares on json-server

--- __json_server_mock__/middleware.js
+++ __json_server_mock__/middleware.js
@@ -1,14 +1,14 @@
 module.exports = (req, res, next) => {
   if (req.method === "POST" && req.path === "/login") {
-    if (req.body.username === "leo" && req.body.password === "123456") {
+    if (req.body.username === "leo" && req.body.password === "321") {
       return res.status(200).json({
         user: {
-          token: "123",
+          token: "321",
         },
       });
     } else {
       return res.status(400).json({
-        message: "username or password errors",
+        message: "wrong user or psd",
       });
     }
   }
